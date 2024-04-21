07e928d53567d506a9a7e8ca1c44765dafb4959f
Hubert <70658889+HubertJan@users.noreply.github.com>
2024-04-21 14:12:07 +0200

    Fix shell scripts

--- server/run_docker.sh
+++ server/run_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sudo docker run --rm --network=host --privileged -d astra-hotspot:v0.1 &&
+sudo rfkill unblock 0 &&
+    sudo docker run --rm --network=host --privileged -d astra-hotspot:v0.1 &&
     sudo docker run --rm -d -p 1883:1883 -p 9001:9001 -d astra-broker:v0.1 &&
-    sudo docker run --rm --network=host --device /dev/gpiomem astra-publisher:v0.1 &&
+    sudo docker run --rm -d --network=host --device /dev/gpiomem astra-publisher:v0.1 &&
     sudo docker run --rm -d -p 8000:8000 -d --mount type=bind,src=/images,target=/fileserver astra-file-server:v0.1
