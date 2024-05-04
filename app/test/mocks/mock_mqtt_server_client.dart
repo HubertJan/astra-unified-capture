711a5b83eb861438431be68917c9af263d41638c
Hubert <70658889+HubertJan@users.noreply.github.com>
2024-05-04 21:09:04 +0200

    Add mocktails; Some tests

--- app/test/mocks/mock_mqtt_server_client.dart
+++ app/test/mocks/mock_mqtt_server_client.dart
@@ -1,4 +1,48 @@
-import 'package:mocktail/mocktail.dart';
-import 'package:mqtt_client/mqtt_server_client.dart';
+import 'dart:convert';
 
-class MockMqttServerClient extends Mock implements MqttServerClient {}
+import 'package:mocktail/mocktail.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+import 'package:typed_data/typed_buffers.dart';
+
+class MockMqttServerClient extends Mock implements MqttServerClient {
+  void mockConnectionStatus(MqttConnectionState state) {
+    when(() => connect()).thenAnswer((_) async {
+      final s = MqttClientConnectionStatus();
+      s.state = state;
+      return s;
+    });
+
+    when(() => connectionStatus).thenAnswer((_) {
+      final s = MqttClientConnectionStatus();
+      s.state = state;
+      return s;
+    });
+  }
+
+  MockMqttServerClient() {
+    when(() => subscribe(any(), any())).thenAnswer((_) {
+      return Subscription();
+    });
+  }
+}
+
+class FakeMqttReceivedMessage extends Mock
+    implements MqttReceivedMessage<MqttMessage> {
+  FakeMqttReceivedMessage();
+
+  factory FakeMqttReceivedMessage.prepareWithMessage(String messageContent) {
+    final message = FakeMqttReceivedMessage();
+    final publishMessage = FakeMqttPublishMessage();
+    final payload = FakeMqttPublishPayload();
+    when(() => payload.message)
+        .thenReturn(Uint8Buffer()..addAll(utf8.encode(messageContent)));
+    when(() => publishMessage.payload).thenReturn(payload);
+    when(() => message.payload).thenReturn(publishMessage);
+    return message;
+  }
+}
+
+class FakeMqttPublishPayload extends Mock implements MqttPublishPayload {}
+
+class FakeMqttPublishMessage extends Mock implements MqttPublishMessage {}
--- app/test/service_tests/command_receiver_test.dart
+++ app/test/service_tests/command_receiver_test.dart
@@ -1,22 +1,11 @@
-import 'dart:convert';
+import 'dart:async';
 
-import 'package:app/service_provider/network_info_provider.dart';
 import 'package:app/services/command_receiver.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:mqtt_client/mqtt_client.dart';
-import 'package:typed_data/typed_buffers.dart';
 
 import '../mocks/mock_mqtt_server_client.dart';
-import '../mocks/network_info_mock.dart';
-import '../utils/create_container.dart';
-
-class FakeMqttReceivedMessage extends Mock
-    implements MqttReceivedMessage<MqttMessage> {}
-
-class FakeMqttPublishPayload extends Mock implements MqttPublishPayload {}
-
-class FakeMqttPublishMessage extends Mock implements MqttPublishMessage {}
 
 void main() {
   setUpAll(() {
@@ -27,40 +16,45 @@ void main() {
     final mockClient = MockMqttServerClient();
     final receiver = CommandReceiver(client: mockClient);
 
-    when(() => mockClient.connect()).thenAnswer((_) async {
-      final s = MqttClientConnectionStatus();
-      s.state = MqttConnectionState.connected;
-      return s;
-    });
-
-    when(() => mockClient.connectionStatus).thenAnswer((_) {
-      final s = MqttClientConnectionStatus();
-      s.state = MqttConnectionState.connected;
-      return s;
-    });
-
-    when(() => mockClient.subscribe(any(), any())).thenAnswer((_) {
-      return Subscription();
-    });
+    mockClient.mockConnectionStatus(MqttConnectionState.connected);
 
     bool isRecording = false;
     receiver.onRecordingUpdate = (isRec) {
       isRecording = isRec;
     };
-    final msg = FakeMqttReceivedMessage();
-    final pubMsg = FakeMqttPublishMessage();
-    final payload = FakeMqttPublishPayload();
-    when(() => payload.message)
-        .thenReturn(Uint8Buffer()..addAll(utf8.encode("ON")));
-    when(() => pubMsg.payload).thenReturn(payload);
-    when(() => msg.payload).thenReturn(pubMsg);
 
     when(() => mockClient.updates).thenAnswer((invocation) async* {
-      yield [msg];
+      yield [FakeMqttReceivedMessage.prepareWithMessage("ON")];
     });
 
     await receiver.connect();
     await Future.delayed(Duration.zero);
     expect(isRecording, true);
+  });
+
+  test("Check if onRecordingUpdate is triggered with correct values", () async {
+    final mockClient = MockMqttServerClient();
+    final receiver = CommandReceiver(client: mockClient);
+    mockClient.mockConnectionStatus(MqttConnectionState.connected);
+
+    bool isRecording = false;
+    receiver.onRecordingUpdate = (isRec) {
+      isRecording = isRec;
+    };
+
+    final messageStream = StreamController();
+    when(() => mockClient.updates).thenAnswer((invocation) async* {
+      await for (final message in messageStream.stream) {
+        yield [message];
+      }
+    });
+
+    await receiver.connect();
+    messageStream.add(FakeMqttReceivedMessage.prepareWithMessage("ON"));
+    await Future.delayed(Duration.zero);
+    expect(isRecording, true);
+    messageStream.add(FakeMqttReceivedMessage.prepareWithMessage("OFF"));
+    await Future.delayed(Duration.zero);
+    expect(isRecording, false);
   });
 }
